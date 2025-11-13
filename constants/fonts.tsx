932206e2e06d301958363b859efbae37bebc0d07
AhmedGit-Bit <ahmedk.elsheshtawy@gmail.com>
2025-11-13 13:50:29 +0300

    Added Fonts And Font Imports

---
+++ constants/fonts.tsx
@@ -0,0 +1,15 @@
+import { default as poppinsBold, default as poppinsExtraBold } from "@/assets/fonts/poppins/Poppins-ExtraBold.ttf";
+import poppinsExtraLight from "@/assets/fonts/poppins/Poppins-ExtraLight.ttf";
+import poppinsLight from "@/assets/fonts/poppins/Poppins-Light.ttf";
+import poppinsThin from "@/assets/fonts/poppins/Poppins-Thin.ttf";
+import poppinsThinItalic from "@/assets/fonts/poppins/Poppins-ThinItalic.ttf";
+
+
+export const fonts={
+    poppinsBold,
+    poppinsExtraBold,
+    poppinsLight,
+    poppinsExtraLight,
+    poppinsThin,
+    poppinsThinItalic
+}
